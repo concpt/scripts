6165401a70568eb544b70cb9b15ffefcda20275c
concpt <93624093+concpt@users.noreply.github.com>
2023-06-06 22:11:23 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update -y && \
 apt upgrade -y && \
-apt install sudo nload vnstat unbound curl ufw&& \
+apt install sudo nload vnstat unbound curl ufw && \
 echo "nameserver 127.0.0.1" > /etc/resolv.conf && \
 useradd -m concept -s /bin/bash && \
 usermod -aG sudo concept && \
